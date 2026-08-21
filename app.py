51a234dbc8035c7b052fcdf21d6f8169c3eeb695
singhnaisha02-oss <singhnaisha02@gmail.com>
2026-08-21 14:07:51 +0530

    Create Streamlit app for RAG with ChatGroq
    Implemented a Streamlit app that utilizes the ChatGroq model to answer user queries based on a provided context about NEPL.

---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+import streamlit as st
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_groq import ChatGroq  # Groq import kiya
+
+st.set_page_config(
+    page_title="My Mobile RAG App", page_icon="🤖", layout="centered"
+)
+st.title("🤖 My RAG App (Powered by Groq)")
+st.write("Free Llama model ke sath!")
+
+# Yahan apni gsk_ se shuru hone wali Groq key daal do
+llm = ChatGroq(
+    model="openai/gpt-oss-20b",
+    temperature=0,
+    groq_api_key="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",
+)
+
+context_text = """
+NEPL  is a mining company engaged in a coal mining and coal logistics contract under Central Coalfields Limited.
+
+Its work includes:
+
+Coal production using excavators and other heavy earth-moving machinery at the mine.
+
+Loading of raw coal into tippers.
+
+Transportation of coal from the mine to Kedla Washery, where the coal is washed and processed. 
+
+Managing the tipper fleet, machine deployment, production targets, and coordination with mine and washery authorities.
+
+Contract administration, including correspondence with the Project Officer, billing, statutory compliance, equipment financing, and liaison with banks and government authorities.
+Head Office: Hirak Road, Dhanbad, Jharkhand
+"""
+
+user_query = st.text_input("Apna sawaal pucho:")
+
+if user_query:
+  with st.spinner("Thinking..."):
+    template = """Answer the question based only on the following context:
+        {context}
+
+        Question: {question}
+        """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    rag_chain = (
+        {"context": lambda x: context_text, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    response = rag_chain.invoke(user_query)
+    st.success("Done!")
+    st.write("### Answer:")
+    st.write(response)
